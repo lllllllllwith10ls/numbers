983656ad2b96e2ca39589106f160c6890a8e98a3
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-06-12 16:55:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ class Number {
 	}
 	clean() {
 		if(this.type === "number") {
-			if(this.value) {
+			if(this.value != undefined) {
 				if(this.value > 1e6) {
 					return new Number("10^"+Math.log10(this.value).toString());
 				}
@@ -23,21 +23,21 @@ class Number {
 		if(this.magnitude) {
 			return "10^"+this.magnitude.toString();
 		} else {
-			return Math.floor(this.number).toString();
+			return Math.floor(this.value).toString();
 		}
 	}
 	static comp(a,b) {
-		if(a.number != undefined && b.number != undefined) {
-			if(a.number > b.number) {
+		if(a.value != undefined && b.value != undefined) {
+			if(a.value > b.value) {
 				return a;
-			} else if(a.number < b.number) {
+			} else if(a.value < b.value) {
 				return b;
 			} else {
 				return "equal";
 			}
-		} else if(a.number != undefined && b.number == undefined) {
+		} else if(a.value != undefined && b.value == undefined) {
 			return b;
-		} else if(b.number != undefined && a.number == undefined) {
+		} else if(b.value != undefined && a.value == undefined) {
 			return a;
 		} else {
 			if(Number.comp(a.magnitude,b.magnitude) === a.magnitude) {
@@ -62,7 +62,7 @@ function update() {
 	add2 += add3;
 	add += add2;
 	if(phase === 0) {
-		number.number += add;
+		number.value += add;
 	}
 	number = number.clean();
 	document.getElementById("number").innerHTML = number.toString();
